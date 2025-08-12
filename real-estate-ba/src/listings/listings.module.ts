40edb4d6764ea20c0767994a9b26c6b2d08188c8
kzmfhm <khuzaimanazik4@gmail.com>
2025-08-12 21:54:53 +0500

    fix error in modules

--- real-estate-ba/src/listings/listings.module.ts
+++ real-estate-ba/src/listings/listings.module.ts
@@ -1,9 +1,19 @@
+// src/listings/listings.module.ts
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ListingsService } from './listings.service';
 import { ListingsController } from './listings.controller';
+import { Listing } from './entities/listing.entity';
+import { Agent } from '../agents/entities/agent.entity';
+import { ListingField } from './entities/listing-field.entity';
+
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Listing, Agent, ListingField])
+  ],
+  controllers: [ListingsController],
   providers: [ListingsService],
-  controllers: [ListingsController]
 })
 export class ListingsModule {}
