26673cd0ccaafc99f7c0337939d9321fa605a8dc
kzmfhm <khuzaimanazik4@gmail.com>
2025-09-17 19:55:41 +0500

    add commit

--- real-estate-ba/src/fields/dto/create-field.dto.ts
+++ real-estate-ba/src/fields/dto/create-field.dto.ts
@@ -1,11 +1,11 @@
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { FieldType } from '../entities/field.entity';
 
+
 export class CreateFieldDto {
   @IsString()
   @IsNotEmpty()
   name: string;
-
   @IsEnum(FieldType)
   type: FieldType;
 }
